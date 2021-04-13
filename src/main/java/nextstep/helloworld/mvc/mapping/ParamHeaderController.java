5edaa7c2afb1eebe5c89c943097e5d3cc568eb07
hoony <younghoon.kwon91@gmail.com>
2021-04-13 17:19:41 +0900

    feat: 테스트 진행중

--- src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
@@ -3,13 +3,15 @@ package nextstep.helloworld.mvc.mapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/param-header")
 public class ParamHeaderController {
 
-    public ResponseEntity<String> message() {
+    @GetMapping(path = "/message")
+    public ResponseEntity<String> message(@RequestParam(required = false) String message) {
         return ResponseEntity.ok().body("message");
     }
 
